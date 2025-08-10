78cc92695363639d2640257c4346a1b0f30957e3
yuri <yurigx12345@gmail.com>
2025-08-09 22:53:26 -0300

    Angular-login

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  standalone: true,
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class Login {
+
+}
