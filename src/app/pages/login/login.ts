72480f16420b2b9b9db3035efd1234ba7e794533
yuri <yurigx12345@gmail.com>
2025-08-14 21:39:36 -0300

    📦 NEW:  Implementação da pagina login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   standalone: true,
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class Login {
+  email: string = '';
+  senha: string = '';
 
+  constructor(private router: Router) {}
+
+  entrar() {
+    fetch('http://localhost:8080/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: this.email, senha: this.senha })
+    })
+    .then(async res => {
+      if (res.ok) {
+        alert(await res.text());
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert(await res.text());
+      }
+    })
+    .catch(err => console.error(err));
+  }
 }
