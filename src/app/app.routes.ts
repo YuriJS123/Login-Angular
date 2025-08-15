49e61769858bef89a0cf764df7a988526a584be4
yuri <yurigx12345@gmail.com>
2025-08-14 21:38:26 -0300

    🐛 FIX: Adição de novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-
+import { Dashboard } from './pages/dashboard/dashboard'
 export const routes: Routes = [
+
+  {path: 'dashboard', component: Dashboard},
   { path: '', component: Login },
   { path: '**', redirectTo: '' }
 ];
