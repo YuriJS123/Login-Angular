017edd335a3e688fb76fc0d987df53c92588a2aa
yuri <yurigx12345@gmail.com>
2025-08-25 22:14:51 -0300

    🐛 FIX: Mudanças na rotas das paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-import { Dashboard } from './pages/dashboard/dashboard'
+import { HubDeLinguagens } from './pages/hub-de-linguagens/hub-de-linguagens';
 export const routes: Routes = [
 
-  {path: 'dashboard', component: Dashboard},
+  {path: 'HubDeLinguagens', component: HubDeLinguagens,},
   { path: '', component: Login },
   { path: '**', redirectTo: '' }
 ];
