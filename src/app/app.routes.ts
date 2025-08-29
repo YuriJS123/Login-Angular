24b656a17184ffb829911af295a3f4f8b0c0fa9d
yuri <yurigx12345@gmail.com>
2025-08-28 22:34:56 -0300

    🐛 FIX: Adição da nova rota para a (pagina-html)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { HubDeLinguagens } from './pages/hub-de-linguagens/hub-de-linguagens';
+import { PaginaHtml } from './pages/pagina-html/pagina-html';
+
+
 export const routes: Routes = [
 
   {path: 'HubDeLinguagens', component: HubDeLinguagens,},
+  {path: 'PaginaHtml',component: PaginaHtml,},
   { path: '', component: Login },
   { path: '**', redirectTo: '' }
 ];
