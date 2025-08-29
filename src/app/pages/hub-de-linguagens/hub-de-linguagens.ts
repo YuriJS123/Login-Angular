7a40b6b59f3d527f11001fa917ef3f9ded8fae02
yuri <yurigx12345@gmail.com>
2025-08-28 22:35:34 -0300

    🐛 FIX: Adição de nova rota na pagina hub-de-linguagens

--- src/app/pages/hub-de-linguagens/hub-de-linguagens.ts
+++ src/app/pages/hub-de-linguagens/hub-de-linguagens.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+// importa o AuthService se realmente existir no seu projeto
+// import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-hub-de-linguagens',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './hub-de-linguagens.html',
-  styleUrl: './hub-de-linguagens.css'
+  styleUrls: ['./hub-de-linguagens.css']
 })
 export class HubDeLinguagens {
-
+  constructor(
+    private router: Router, 
+    // private authService: AuthService
+  ) {}
 }
