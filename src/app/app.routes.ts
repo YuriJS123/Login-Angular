4e3d12293030907cbad930fbdd7b6bede2106536
yuri <yurigx12345@gmail.com>
2025-09-02 23:11:01 -0300

    🐛 FIX: Adição de novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { HubDeLinguagens } from './pages/hub-de-linguagens/hub-de-linguagens';
 import { PaginaHtml } from './pages/pagina-html/pagina-html';
-
+import { PaginaCss } from './pages/pagina-css/pagina-css';
+import { Perfil} from './pages/perfil/perfil';
+import { Cadastro} from './pages/cadastro/cadastro';
 
 export const routes: Routes = [
 
+  {path: 'Cadastro', component: Cadastro,},
+  {path: 'Perfil', component: Perfil,},
   {path: 'HubDeLinguagens', component: HubDeLinguagens,},
   {path: 'PaginaHtml',component: PaginaHtml,},
+  {path: 'PaginaCss',component: PaginaCss,},
   { path: '', component: Login },
   { path: '**', redirectTo: '' }
 ];
