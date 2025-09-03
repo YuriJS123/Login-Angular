7ca60ac153e190ebcc7989c83198f2d7918a7bc0
yuri <yurigx12345@gmail.com>
2025-09-02 23:12:14 -0300

    🐛 FIX: Mudanças na pagina hub-de-linguagens

--- src/app/pages/hub-de-linguagens/hub-de-linguagens.ts
+++ src/app/pages/hub-de-linguagens/hub-de-linguagens.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 // importa o AuthService se realmente existir no seu projeto
 // import { AuthService } from '../services/auth.service';
 
@@ -12,8 +13,27 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./hub-de-linguagens.css']
 })
 export class HubDeLinguagens {
+  imagemPreview: string | null = null; 
+
+  
   constructor(
     private router: Router, 
     // private authService: AuthService
   ) {}
+
+  onImagemSelecionada(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const arquivo = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imagemPreview = reader.result as string; // joga a imagem pro preview
+      };
+
+      reader.readAsDataURL(arquivo);
+}
+
+}
 }
